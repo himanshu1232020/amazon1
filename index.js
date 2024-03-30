5061713d4a039cdc4a80167def9b3261df515154
himanshu1232020 <164318065+himanshu1232020@users.noreply.github.com>
2024-03-30 12:46:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+$('button').click(function(){
+
+
+    $('p').html("Hello my name is Suman")
+    // $('p').css("background","green")
+
+    $('p').css(
+        "background","green","color","red")
+})
+
+
+})
